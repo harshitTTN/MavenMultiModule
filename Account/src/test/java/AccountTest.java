cd25ae87bd3b3b64022d8f278d43fa56cb74b30d
Harshit TTN <harshit.abrol@tothenew.com>
2021-02-25 13:31:17 +0530

    First Maven Commit

---
+++ Account/src/test/java/AccountTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert.*;
+
+import static org.junit.Assert.fail;
+
+
+public class AccountTest {
+    @org.junit.Test
+    public void test(){
+//        fail();
+
+    }
+
+}
---
+++ Employee/src/main/java/Emulti/Account.java
@@ -0,0 +1,7 @@
+package Emulti;
+
+public class Account {
+    public static void main(String[] args){
+        System.out.println("This the Account class");
+    }
+}
